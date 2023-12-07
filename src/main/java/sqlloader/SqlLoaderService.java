5d434a39a9fab4afb5a8ab5e17cb2d76fb56262b
Guy <guy_duplantier@marklogic.com>
2023-12-07 17:30:09 -0500

    Update log statements and composite key solution

--- src/main/java/sqlloader/SqlLoaderService.java
+++ src/main/java/sqlloader/SqlLoaderService.java
@@ -17,6 +17,8 @@ import com.marklogic.client.io.DocumentMetadataHandle;
 import com.marklogic.client.io.Format;
 import com.marklogic.client.io.StringHandle;
 
+import java.util.ArrayList;
+
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
@@ -106,13 +108,15 @@ public class SqlLoaderService {
                     logger.info("Batch Success");
                 })
                 .onBatchFailure((batch, throwable) -> {
-                    logger.info("Batch Failure: " + throwable.getMessage());
+                    logger.info("Batch Failure: {}", throwable.getMessage());
                 });
 
         ticket = manager.startJob(mlWriter);
     }
 
     public void load() {
+        logger.info("--------------- Loading data for table: {} ---------------------", tableName);
+
         try (Connection conn = DriverManager.getConnection(dbUrl, username, password);
                 Statement stmt = conn.createStatement();
                 Statement stmt2 = conn.createStatement();
@@ -122,22 +126,13 @@ public class SqlLoaderService {
                                 + tableName + "') AND OWNER = UPPER('" + schemaName + "') AND CONSTRAINT_TYPE = 'P')");
                 ResultSet rs = stmt.executeQuery("select * from " + schemaName + "." + tableName)) {
 
-            logger.info("--------------- Loading data for table: " + tableName + " ---------------");
-            // Get primary key of table to use as unique URI property
-            String uriId = null;
-            if (rs2.next()) {
-                uriId = rs2.getString(1);
-                logger.info("Using value in column: " + uriId + " for unique URI value");
-            } else {
-                logger.info("Using a randomly generated UUID for unique URI value");
-            }
+            ArrayList<String> primaryKeys = queryPrimaryKeys(rs2);
 
             // Oracle default is 10.
             rs.setFetchSize(100);
 
             while (rs.next()) {
-                Object uriIdVal = (uriId == null || uriId.equals("")) ? UUID.randomUUID().toString()
-                        : rs.getObject(uriId);
+                String uriIdVal = buildUriId(rs, primaryKeys);
                 String uri = "/" + sourceName + "/" + schemaName + "/" + tableName + "/" + uriIdVal + ".xml";
                 String xmlResult = ResultSetParserUtil.convertRecordToXML(rs, metadata);
                 StringHandle stringHandle = new StringHandle(xmlResult).withFormat(Format.XML);
@@ -158,5 +153,36 @@ public class SqlLoaderService {
         }
         mlWriter.flushAndWait();
         manager.stopJob(ticket);
+    }
+
+    // Get primary key(s) of table to use as unique URI property
+    // Will handle composite keys by concatenating them
+    private ArrayList<String> queryPrimaryKeys(ResultSet rs) throws SQLException {
+        ArrayList<String> primaryKeys = new ArrayList<>();
+        while (rs.next()) {
+            String colVal = rs.getString(1);
+            primaryKeys.add(colVal);
+            logger.info("Using value in column: {} for unique URI value", colVal);
+        }
+        if (primaryKeys.isEmpty())
+            logger.info("Using a randomly generated UUID for unique URI value");
+
+        return primaryKeys;
+    }
+
+    private String buildUriId(ResultSet rs, ArrayList<String> primaryKeys) throws SQLException {
+        StringBuilder uriIdVal = new StringBuilder();
+        if (primaryKeys.isEmpty()) {
+            uriIdVal.append(UUID.randomUUID().toString());
+        } else {
+            int index = 0;
+            for (String primaryKey : primaryKeys) {
+                uriIdVal.append(rs.getObject(primaryKey));
+                if (index > 1)
+                    uriIdVal.append("-");
+                index++;
+            }
+        }
+        return uriIdVal.toString();
     }
 }
